6727612365ec81aa9450d3990a1acc904373774d
Jeffrey Russ <jeffreylynnruss@gmail.com>
2016-06-10 04:31:08 -0400

    reload bash

--- install.sh
+++ install.sh
@@ -11,7 +11,7 @@ echo
 echo "appending ""$HOME"/.bashrc
 echo " with ""$BRC_STR"
 echo "$BRC_STR" >> "$HOME"/.bashrc
-source "$HOME"/.bashrc
+bash
 echo
 echo "installation complete."
 echo "To view the man page, run:"
